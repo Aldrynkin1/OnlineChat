356839d84a00e97c70ab3f46fb00f4484393223d
Aldrynkin1 <albertdrynkin600@gmail.com>
2026-09-03 20:08:33 +0300

    Add logs to comfortable debugging

---
+++ include/Logger.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Types.hpp"
+
+class Logger
+{
+public:
+    Logger() = default;
+    ~Logger() = default;
+
+    void log(LogLevel::Level level, const std::string &text);
+};
+
+void Logger::log(LogLevel::Level level, const std::string &text)
+{
+    if (level == LogLevel::Level::INFO)
+    {
+        std::cout << "[INFO]: ";
+    }
+    else if (level == LogLevel::Level::WARNING)
+    {
+        std::cout << "[WARNING]: ";
+    }
+    else if (level == LogLevel::Level::ERROR)
+    {
+        std::cout << "[ERROR]: ";
+    }
+    else if (level == LogLevel::Level::FATAL)
+    {
+        std::cout << "[FATAL]: ";
+    }
+
+    std::cout << text.c_str() << std::endl;
+}
